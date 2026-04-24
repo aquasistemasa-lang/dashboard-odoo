4845bee11fb651336e89c00f57c995314d44eb30
aquasistemasa-lang <aquasistemasa@gmail.com>
2026-04-24 14:53:59 -0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,12 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Dashboard Odoo",
+};
+
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html lang="es">
       <body>
         {children}
       </body>
